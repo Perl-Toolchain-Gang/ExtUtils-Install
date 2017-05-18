7eb941d0b471d6ce901c63ff198228cb3b8cd8a3
Yves Orton <demerphq@gmail.com>
2017-05-18 23:41:18 +0200

    bump version of Install.pm

--- lib/ExtUtils/Install.pm
+++ lib/ExtUtils/Install.pm
@@ -32,11 +32,11 @@ ExtUtils::Install - install files from here to there
 
 =head1 VERSION
 
-2.10
+2.11
 
 =cut
 
-our $VERSION = '2.10';  # <-- do not forget to update the POD section just above this line!
+our $VERSION = '2.11';  # <-- do not forget to update the POD section just above this line!
 $VERSION = eval $VERSION;
 
 =pod
