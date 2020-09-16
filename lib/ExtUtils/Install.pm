87f3480f4fce43e027fb6f6d17e9d792f559603e
Graham Knop <haarg@haarg.org>
2020-09-16 15:36:54 +0200

    only try to create each directory once in pm_to_blib
    _mkpath does lots of extra calculations for writability so avoiding
    redundant calls to it can save some time.

--- lib/ExtUtils/Install.pm
+++ lib/ExtUtils/Install.pm
@@ -1199,6 +1199,7 @@ environment variable will silence this output.
 sub pm_to_blib {
     my($fromto,$autodir,$pm_filter) = @_;
 
+    my %dirs;
     _mkpath($autodir,0,0755) if defined $autodir;
     while(my($from, $to) = each %$fromto) {
         if( -f $to && -s $from == -s $to && -M $to < -M $from ) {
@@ -1222,7 +1223,10 @@ sub pm_to_blib {
             # we wont try hard here. its too likely to mess things up.
             forceunlink($to);
         } else {
-            _mkpath(dirname($to),0,0755);
+            my $dirname = dirname($to);
+            if (!$dirs{$dirname}++) {
+                _mkpath($dirname,0,0755);
+            }
         }
         if ($need_filtering) {
             run_filter($pm_filter, $from, $to);
